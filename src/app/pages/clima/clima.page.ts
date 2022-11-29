ba3076ab452b5b23bb0204618c458a9f0b2a9175
saitanax <nomaslimites@gmail.com>
2022-11-29 13:40:13 -0300

    Implementamos geolocalizacion
    Co-Authored-By: Kuro <114423693+KuroOno@users.noreply.github.com>

--- src/app/pages/clima/clima.page.ts
+++ src/app/pages/clima/clima.page.ts
@@ -1,6 +1,7 @@
-import { environment } from 'src/environments/environment';
+import { Component, OnInit } from '@angular/core';
+import { ClimaService } from '../../services/clima.service';
 import {HttpClient} from '@angular/common/http'
-import { Component} from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
@@ -10,18 +11,53 @@ const API_KEY = environment.API_KEY;
   templateUrl: './clima.page.html',
   styleUrls: ['./clima.page.scss'],
 })
-export class ClimaPage {
-  pageTitle = 'Clima'
+export class ClimaPage implements OnInit {
+  pageTitle = 'Clima';
+  isNotLogin = true;
+  city = 'Santiago';
+  hola: number;
+  hasPermission = false;
+  latitude: number;
+  longitude: number;
+  temp: number;
+  statusDescription: string;
+  weatherLoaded: boolean;
   weatherTemp:any
   todayDate = new Date()
   cityName: any
   weatherIcon: any
   weatherDetails:any
-  constructor(public httpClient:HttpClient) { 
+  constructor(private weatherService: ClimaService, public httpClient:HttpClient ) {
+    navigator.geolocation.getCurrentPosition(
+      (geo) => this.geoLocationSuccess(geo),
+      (err) => this.geoLocationError(err)
+    );
     this.loadData()
-}
+  }
+  geoLocationSuccess(geo) {
+    this.hasPermission = true;
+    this.latitude = geo.coords.latitude;
+    this.longitude = geo.coords.longitude;
+    this.loadWeather();
+  }
+  loadWeather() {
+    this.weatherLoaded = false;
+    this.weatherService
+      .getCurrentWheater(this.latitude, this.longitude)
+      .then((res) => {
+        console.log(res);
+        this.city = res.name;
+        this.temp = res.main.temp;
+        this.statusDescription = res.weather[0]?.description.toUpperCase();
+        this.weatherLoaded = true;
+      });
+  }
+  geoLocationError(r) {
+    this.hasPermission = false;
+  }
+  
   loadData(){
-    this.httpClient.get(`${API_URL}/weather?q=${"Santiago"}&appid=${API_KEY}`).subscribe(results=>{
+    this.httpClient.get(`${API_URL}/weather?q=${this.city}&appid=${API_KEY}`).subscribe(results=>{
       //console.log(results);
       this.weatherTemp = results['main']
       this.cityName = results['name']
@@ -31,4 +67,5 @@ export class ClimaPage {
       this.weatherIcon = `http://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`
     })
   }
+  ngOnInit() {}
 }
---
+++ src/app/services/clima.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClimaService {
+  constructor() {}
+  getCurrentWheater(latitude: number, longitude: number) {
+    const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${environment.API_KEY}&lang=sp&units=metric`;
+    console.log({ url });
+    return fetch(
+     url
+    ).then((res) => res.json());
+  }
+}
