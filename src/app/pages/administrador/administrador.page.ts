e5c807e6d1da25c0fa6aa04721ffbb3664c1b3c5
saitanax <nomaslimites@gmail.com>
2022-12-14 19:03:30 -0300

    Rework adminstrador
    Co-Authored-By: Kuro <114423693+KuroOno@users.noreply.github.com>

--- src/app/pages/administrador/administrador.page.ts
+++ src/app/pages/administrador/administrador.page.ts
@@ -1,15 +1,151 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { AsignaturasService } from 'src/app/services/asignaturas.service';
+import { ValidacionesService } from 'src/app/services/validaciones.service';
+import { LoadingController, ToastController} from '@ionic/angular';
+
+import { FirebaseService} from '../../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-administrador',
   templateUrl: './administrador.page.html',
   styleUrls: ['./administrador.page.scss'],
 })
-export class AdministradorPage implements OnInit {
-
-  constructor() { }
+export class AdministradorPage implements OnInit {        
+  pageTitle= 'Administrador';
+  
+  //variable grupo:
+  usuario = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,2}.[0-9]{3}.[0-9]{3}-[0-9kK]')]),
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    ap_paterno: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    fecha_nac: new FormControl('', [Validators.required]),
+    //solo se validan correos de alumnos.
+    correo: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@(duocuc).(cl)')]),
+    clave: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(18)]),
+    //el tipo de usuario cuando se registrar en solo del tipo alumno.
+    tipo_usuario: new FormControl('alumno', [Validators.required]),
+  });
+  repetir_clave: string;
+  usuarioEncontrado: any = ''
+  usuarios : any [] = [];
+   
+  asignaturas: any [] = []
+  id_modificar: any = '';
+  handlerMessage = '';
+  roleMessage = ''
+  constructor(private validaciones: ValidacionesService, private router: Router,
+    private FirebaseService : FirebaseService, private alertController:AlertController, private toast: ToastController) { }
 
   ngOnInit() {
+    this.cargarDatos()
+
   }
+cargarDatos(){
+  this.FirebaseService.getUsuarios('usuarios').subscribe(
+    data => {
+      this.usuarios = [];
+      for(let u of data){
+        let usu = u.payload.doc.data();
+        usu['id'] = u.payload.doc.id;
+        this.usuarios.push( usu );
+      }
+      console.log(this.usuarios)
+    }
+  );
+}
+async eliminar(id){
+  
+  const alert = await this.alertController.create({
+    header: 'Alert!',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          this.handlerMessage = 'Alert canceled';
+        },
+      },
+      {
+        text: 'OK',
+        role: 'confirm',
+        handler: () => {
+          this.FirebaseService.eliminar('usuarios', id);
+        },
+      },
+    ],
+  })
+
+  await alert.present();
+
+  const { role } = await alert.onDidDismiss();
+  this.roleMessage = `Dismissed with role: ${role}`;
+}
+registrar(){
+  if (!this.validaciones.validarRut(this.usuario.controls.rut.value)) {
+    alert('Rut incorrecto!');
+    return;
+  }
+  if (!this.validaciones.validarEdadMinima(17, this.usuario.controls.fecha_nac.value)) {
+    alert('Edad mínima 17 años!');
+    return;
+  }
+  if (this.usuario.controls.clave.value != this.repetir_clave) {
+    alert('Contraseñas no coinciden!');
+    return;
+  }
+this.usuarioEncontrado = this.usuarios.find(usu => usu.rut == this.usuario.value.rut)
+
+  if(this.usuarioEncontrado == undefined){
+    this.FirebaseService.agregar('usuarios', this.usuario.value)
+  this.toastexito('bottom', 'usuario Registrado');
+
+}else{
+  this.toastError('bottom', 'usuario ya existe');
+}
+}
+
+async toastError(position: 'bottom', message: string) {
+  const toast = await this.toast.create({
+    message: message,
+    duration: 3000,
+    position: position,
+    icon: 'skull-outline'
+  });
+  toast.present();
+  }
+  async toastexito(position: 'bottom', message: string) {
+  const toast = await this.toast.create({
+    message: message,
+    duration: 3000,
+    position: position,
+    icon: 'skull-outline'
+  });
+  toast.present();
+  }
+
+buscar(id){
+  this.FirebaseService.getDato('usuarios', id).subscribe(
+    (response: any) => {
+      //console.log( response.data() );
+      this.usuario.setValue( response.data() );
+      this.id_modificar = response.id;
+    }
+  );
+}
+modificar(){
+  let asig = this.usuario.value;
+  this.FirebaseService.modificar('usuarios', this.id_modificar, asig);
+  this.usuario.reset();
+  this.id_modificar = '';
+}
+
+
+
+
+
+
 
 }
--- src/app/services/asignaturas.service.ts
+++ src/app/services/asignaturas.service.ts
@@ -1,9 +1,115 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { AngularFirestore} from '@angular/fire/compat/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsignaturasService {
 
-  constructor() { }
+  asignaturas: any[] = [];
+
+  constructor(private asignaturaStorage: Storage, private fire: AngularFirestore) { 
+    asignaturaStorage.create();
+  }
+
+  //MÉTODOS DEL CRUD DEL STORAGE:
+  async agregar(key, Asignatura){
+    this.asignaturas = await this.asignaturaStorage.get(key) || [];
+    
+    //VAMOS A VER SI EL DATO QUE VIENE COMO PARÁMETRO TIENE id:
+    //si tiene id, buscamos si existe, si NO tiene id, agregamos:
+    if(Asignatura.id == ''){
+      var id = this.asignaturas.length + 1;
+      Asignatura.id = id;
+      this.asignaturas.push(Asignatura);
+      await this.asignaturaStorage.set(key, this.asignaturas);
+      return true;
+    }
+    return false;
+  }
+  
+  async getDato(key, identificador){
+    this.asignaturas = await this.asignaturaStorage.get(key) || [];
+    return this.asignaturas.find(Asignatura => Asignatura.profesor == identificador);
+  }
+
+  async getDatos(key){
+    this.asignaturas = await this.asignaturaStorage.get(key) || [];
+    return this.asignaturas;
+  }
+
+  async eliminar(key, identificador){
+    this.asignaturas = await this.asignaturaStorage.get(key) || [];
+
+    this.asignaturas.forEach((value, index) => {
+      if(value.id == identificador){
+        this.asignaturas.splice(index, 1);
+      }
+    });
+
+    await this.asignaturaStorage.set(key, this.asignaturas);
+  }
+
+  async actualizar(key, dato){
+    this.asignaturas = await this.asignaturaStorage.get(key) || [];
+
+    var index = this.asignaturas.findIndex(value => value.id == dato.id);
+    this.asignaturas[index] = dato;
+
+    await this.asignaturaStorage.set(key, this.asignaturas);
+  }
+
+  agregarAsignatura(coleccion, value){
+    try {
+
+      return this.fire.collection(coleccion).add(value);
+
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  buscarAsignatura(coleccion, id){
+    try {
+      return this.fire.collection(coleccion).doc(id).get();
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  buscarTodo(coleccion){
+    try {
+      return this.fire.collection(coleccion).snapshotChanges();
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  modificar(coleccion, id, value){
+    try {
+      this.fire.collection(coleccion).doc(id).set(value);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  eliminarAsignatura(coleccion, id){
+    try {
+      this.fire.collection(coleccion).doc(id).delete();
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  
+
+
+  
 }
